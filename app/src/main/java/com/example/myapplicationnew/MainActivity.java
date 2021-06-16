6c87b1463640d8c0046fd113dd8642ef9c86d92e
roii31-game <74428415+roii31-game@users.noreply.github.com>
2021-06-16 09:49:40 -0500

    JSON list android studio

---
+++ app/src/main/java/com/example/myapplicationnew/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.myapplicationnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+
+
+    private void jsonParse(){
+        String url = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
+
+// I don't know what my problem is here, i've declared everything and nothing is showing up
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("");
+                    for (int i = 0; i < jsonArray.length(); i++){
+                        JSONObject list = jsonArray.getJSONObject(i);
+
+                        int id = list.getInt("id");
+                        int listId = list.getInt("listId");
+                        String name = list.getString("name");
+
+                        mTextViewResult.append(String.valueOf(id) + ", " + String.valueOf(listId) + ", " + name + "\n\n");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+}
